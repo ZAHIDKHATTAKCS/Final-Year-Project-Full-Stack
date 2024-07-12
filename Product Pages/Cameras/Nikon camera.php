8d78d46a5e27a72b4389619cec4676d81158c49f
zahidkhattakcs <zahidkhattakcs@gmail.com>
2024-07-12 11:22:06 +0500

    add product laptops to DB done!

--- Product Pages/Cameras/Nikon camera.php
+++ Product Pages/Cameras/Nikon camera.php
@@ -1,3 +1,8 @@
+<?php
+  include ('../../DBconnection.php');
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
--- Seller/Dashboard/Add Products/laptops.php
+++ Seller/Dashboard/Add Products/laptops.php
@@ -1,15 +1,15 @@
-<?php 
-    session_start();
-    include ('../../../DBconnection.php');
+<?php
+session_start();
+include '../../../DBconnection.php';
 
-    if(!isset($_SESSION['Seller_Pic'])){
-        ?>
+if (!isset($_SESSION['Seller_Pic'])) {
+    ?>
         <script>
-          alert('First Login Please!');
-          location.replace('../../seller signin.php');
+        alert('First Login Please!');
+        location.replace('../../seller signin.php');
         </script>
         <?php
-      }
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -140,24 +140,25 @@
             <!-- Add product section -->
             <div class="col-10 mt-5 border border border-dark rounded-3 shadow-lg mb-3 h-100 overflow-scroll">
                 <img src="../../../assets/Icons/Laptops/Apple Laptops/1.jpg" alt="" class="w-100" />
+
                 <!-- Add product Form -->
-                <form class="mb-3 text-dark rounded-2 mt-3 p-2">
+                <form class="mb-3 text-dark rounded-2 mt-3 p-2" method="POST" enctype="multipart/form-data">
                     <!-- Laptop title -->
                     <div class="input-group mb-3">
                         <i class="fas fa-laptop input-group-text"></i>
-                        <input type="text" name="" class="form-control" placeholder="Laptop Title" required />
+                        <input type="text" name="Laptop_Title" class="form-control" placeholder="Laptop Title" required />
                     </div>
 
                     <!-- Laptop Description -->
                     <div class="input-group mb-3">
                         <i class="fas fa-laptop input-group-text"></i>
-                        <input type="text" name="" class="form-control" placeholder="Laptop Description" required />
+                        <input type="text" name="Laptop_Description" class="form-control" placeholder="Laptop Description" required />
                     </div>
 
                      <!-- Laptop quantity -->
                        <div class="input-group mb-3">
                         <i class="fa-solid fa-plus-minus input-group-text"></i>
-                        <input type="number" name="" class="form-control" placeholder="Enetr quantity" required />
+                        <input type="number" name="Quantity" class="form-control" placeholder="Enetr quantity" required />
                        </div>
 
                        <!-- Laptop price -->
@@ -168,26 +169,28 @@
 
                     <!-- Laptop pic 1 -->
                     <div class="input-group mb-3">
-                        <input type="file" name="" class="form-control" required />
+                        <input type="file" name="Laptop_Pic_1" class="form-control" required />
                     </div>
 
                     <!-- Laptop pic 2 -->
                     <div class="input-group mb-3">
-                        <input type="file" name="" class="form-control" required />
+                        <input type="file" name="Laptop_Pic_1" class="form-control" required />
                     </div>
 
                     <!-- Laptop pic 3 -->
                     <div class="input-group mb-3">
-                        <input type="file" name="" class="form-control" required />
+                        <input type="file" name="Laptop_Pic_1" class="form-control" required />
                     </div>
 
                     <!-- Add product Button -->
                     <div class="d-flex justify-content-center">
-                        <button type="submit" class="mt-3 btn btn-success w-100">
+                        <button type="submit" name="submit" class="mt-3 btn btn-success w-100">
                             Add Product
                         </button>
                     </div>
                 </form>
+
+
             </div>
         </div>
     </div>
@@ -226,40 +229,40 @@
                     <!-- Laptop title -->
                     <div class="input-group mb-3">
                         <i class="fas fa-laptop input-group-text"></i>
-                        <input type="text" name="laptop_title" class="form-control" placeholder="Laptop Title" required />
+                        <input type="text" name="Laptop_Title" class="form-control" placeholder="Laptop Title" required />
                     </div>
 
                     <!-- Laptop Description -->
                     <div class="input-group mb-3">
                         <i class="fas fa-laptop input-group-text"></i>
-                        <input type="text" name="laptop_description" class="form-control" placeholder="Laptop Description" required />
+                        <input type="text" name="Laptop_Description" class="form-control" placeholder="Laptop Description" required />
                     </div>
 
-                      <!-- Laptop quantity -->
-                      <div class="input-group mb-3">
+                    <!-- Laptop quantity -->
+                    <div class="input-group mb-3">
                         <i class="fa-solid fa-plus-minus input-group-text"></i>
-                        <input type="number" name="Laptop_Quantity" class="form-control" placeholder="Enter quantity" required />
-                       </div>
+                        <input type="number" name="Quantity" class="form-control" placeholder="Enter quantity" required />
+                    </div>
 
-                       <!-- Laptop price -->
-                      <div class="input-group mb-3">
+                    <!-- Laptop price -->
+                    <div class="input-group mb-3">
                         <i class="fa-solid fa-dollar-sign input-group-text"></i>
                         <input type="number" name="Laptop_Price" class="form-control" placeholder="Enter Price" required />
-                       </div>
+                    </div>
 
                     <!-- Laptop pic 1 -->
                     <div class="input-group mb-3">
-                        <input type="file" name="laptop_pic_1" class="form-control" required />
+                        <input type="file" name="Laptop_Pic_1" class="form-control" required />
                     </div>
 
                     <!-- Laptop pic 2 -->
                     <div class="input-group mb-3">
-                        <input type="file" name="laptop_pic_1" class="form-control" required />
+                        <input type="file" name="Laptop_Pic_2" class="form-control" required />
                     </div>
 
                     <!-- Laptop pic 3 -->
                     <div class="input-group mb-3">
-                        <input type="file" name="laptop_pic_1" class="form-control" required />
+                        <input type="file" name="Laptop_Pic_3" class="form-control" required />
                     </div>
 
                     <!-- Add product Button -->
@@ -269,6 +272,71 @@
                         </button>
                     </div>
                 </form>
+
+                <!-- PHP Code for form submission -->
+                <?php
+if (isset($_POST['submit'])) {
+
+    $Laptop_Title = $_POST['Laptop_Title'];
+    $Laptop_Description = $_POST['Laptop_Description'];
+    $Laptop_Quantity = $_POST['Quantity'];
+    $Laptop_Price = $_POST['Laptop_Price'];
+
+    // To deals with pictures we must used $_FILES
+    $Laptop_Pic_1 = $_FILES['Laptop_Pic_1'];
+    $Laptop_Pic_2 = $_FILES['Laptop_Pic_2'];
+    $Laptop_Pic_3 = $_FILES['Laptop_Pic_3'];
+
+    // Details of Pic 1
+    $Laptop_Pic_1_Name = $Laptop_Pic_1['name'];
+    $Laptop_Pic_1_Path = $Laptop_Pic_1['tmp_name'];
+    $Laptop_Pic_1_Error = $Laptop_Pic_1['error'];
+
+    // Details of Pic 2
+    $Laptop_Pic_2_Name = $Laptop_Pic_2['name'];
+    $Laptop_Pic_2_Path = $Laptop_Pic_2['tmp_name'];
+    $Laptop_Pic_2_Error = $Laptop_Pic_2['error'];
+
+    // Details of Pic 3
+    $Laptop_Pic_3_Name = $Laptop_Pic_3['name'];
+    $Laptop_Pic_3_Path = $Laptop_Pic_3['tmp_name'];
+    $Laptop_Pic_3_Error = $Laptop_Pic_3['error'];
+
+    if ($Laptop_Pic_1_Error == 0 && $Laptop_Pic_2_Error == 0 && $Laptop_Pic_3_Error == 0) {
+
+        $Final_Destination_Laptop__Pic_1 = 'Add Laptops/' . $Laptop_Pic_1_Name;
+
+        $Final_Destination_Laptop_Pic_2 = 'Add Laptops/' . $Laptop_Pic_2_Name;
+
+        $Final_Destination_Laptop_Pic_3 = 'Add Laptops/' . $Laptop_Pic_3_Name;
+
+        move_uploaded_file($Laptop_Pic_1_Path, $Final_Destination_Laptop__Pic_1);
+
+        move_uploaded_file($Laptop_Pic_2_Path, $Final_Destination_Laptop_Pic_2);
+
+        move_uploaded_file($Laptop_Pic_3_Path, $Final_Destination_Laptop_Pic_3);
+
+        $insertqry = "INSERT INTO `laptops table`(`Laptop Title`, `Laptop Description`, `Laptop_Quantity`, `Picture 1`, `Picture 2`, `Picture 3`) VALUES ('$Laptop_Title','$Laptop_Description','$Laptop_Quantity','$Final_Destination_Laptop__Pic_1','$Final_Destination_Laptop_Pic_2','$Final_Destination_Laptop_Pic_3')";
+
+        $firqery = mysqli_query($conn, $insertqry);
+        if ($insertqry) {
+            ?>
+                            <script>
+                                alert('New Laptop Added to Laptop Category!');
+                            </script>
+                            <?php
+} else {
+            ?>
+                            <script>
+                                alert('Error in Laptop Insertion Query');
+                            </script>
+                            <?php
+}
+    }
+
+    // TODO Add Products Laptops insertion Done ! Camera , headphones , Laptops stands, Monitors and computer are still remains
+}
+?>
             </div>
         </div>
     </div>
